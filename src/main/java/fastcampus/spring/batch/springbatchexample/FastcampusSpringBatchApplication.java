8a4e9bccfed2565b4dec36174fd3234933f2c89b
Jeong Jin Ung <jinung.dev@gmail.com>
2022-07-19 00:10:23 +0900

    batch

--- src/main/java/fastcampus/spring/batch/springbatchexample/FastcampusSpringBatchApplication.java
+++ src/main/java/fastcampus/spring/batch/springbatchexample/FastcampusSpringBatchApplication.java
@@ -1,9 +1,11 @@
 package fastcampus.spring.batch.springbatchexample;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class FastcampusSpringBatchApplication {
 
     public static void main(String[] args) {
--- src/main/java/fastcampus/spring/batch/springbatchexample/part1/HelloConfiguration.java
+++ src/main/java/fastcampus/spring/batch/springbatchexample/part1/HelloConfiguration.java
@@ -1,2 +1,43 @@
-package fastcampus.spring.batch.springbatchexample.part1;public class HelloConfiguration {
+package fastcampus.spring.batch.springbatchexample.part1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class HelloConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public HelloConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job helloJob() { // 스프링배치의 실행 단위
+        return jobBuilderFactory.get("helloJob") // job 이름
+            .incrementer(new RunIdIncrementer()) // 항상 job이 실행할때마다 파라미터 아이디를 자동으로 생성
+            .start(this.helloStep()) // 최초로 실행될 step을 지정하는 메소드
+            .build();
+    }
+
+    @Bean
+    public Step helloStep() { // Job의 실행 단위
+        return stepBuilderFactory.get("helloJob")
+            .tasklet((contribution, chunkContext) -> { // tsklet step 의 실행 단위
+                log.info("hello spring batch");
+                return RepeatStatus.FINISHED;
+            }).build();
+
+    }
+
 }
