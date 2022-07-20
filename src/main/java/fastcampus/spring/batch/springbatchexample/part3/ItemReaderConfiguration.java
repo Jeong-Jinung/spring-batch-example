af76b6c70591ddbe4cc6d680d1281f0ffbded0f9
Jeong Jin Ung <jinung.dev@gmail.com>
2022-07-20 23:29:37 +0900

    batch

--- src/main/java/fastcampus/spring/batch/springbatchexample/part3/ItemReaderConfiguration.java
+++ src/main/java/fastcampus/spring/batch/springbatchexample/part3/ItemReaderConfiguration.java
@@ -3,6 +3,8 @@ package fastcampus.spring.batch.springbatchexample.part3;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,8 +12,17 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @Slf4j
@@ -19,17 +30,27 @@ public class ItemReaderConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+    private final EntityManagerFactory entityManagerFactory;
 
-    public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+    public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory,
+                                    StepBuilderFactory stepBuilderFactory,
+                                    DataSource dataSource,
+                                    EntityManagerFactory entityManagerFactory) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Bean
-    public Job itemReaderJob() {
+    public Job itemReaderJob() throws Exception {
         return this.jobBuilderFactory.get("itemReaderJob")
             .incrementer(new RunIdIncrementer())
             .start(this.customItemReaderStep())
+            .next(this.csvFileStep())
+            .next(this.jdbcStep())
+            .next(this.jpaStep())
             .build();
     }
 
@@ -42,10 +63,87 @@ public class ItemReaderConfiguration {
             .build();
     }
 
+    @Bean
+    public Step csvFileStep() throws Exception {
+        return stepBuilderFactory.get("csvFileStep")
+            .<Person, Person>chunk(10)
+            .reader(this.csvFileItemReader())
+            .writer(itemWriter())
+            .build();
+    }
+
+    @Bean
+    public Step jdbcStep() throws Exception {
+        return stepBuilderFactory.get("jdbcStep")
+            .<Person, Person>chunk(10)
+            .reader(jdbcCursorItemReader())
+            .writer(itemWriter())
+            .build();
+    }
+
+    @Bean
+    public Step jpaStep() throws Exception {
+        return stepBuilderFactory.get("jpaStep")
+            .<Person, Person>chunk(10)
+            .reader(this.jpaCursorItemReader())
+            .writer(itemWriter())
+            .build();
+    }
+
+    private JpaCursorItemReader<Person> jpaCursorItemReader() throws Exception {
+        JpaCursorItemReader<Person> itemReader = new JpaCursorItemReaderBuilder<Person>()
+            .name("japCursorItemReader")
+            .entityManagerFactory(entityManagerFactory)
+            .queryString("select p from Person p")
+            .build();
+        itemReader.afterPropertiesSet();
+        return itemReader;
+    }
+
+    private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
+        JdbcCursorItemReader<Person> itemReader = new JdbcCursorItemReaderBuilder<Person>()
+            .name("jdbcCursorItemReader")
+            .dataSource(dataSource)
+            .sql("select id, name, age, address from person")
+            .rowMapper((rs, rowNum) -> new Person(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)))
+            .build();
+
+        itemReader.afterPropertiesSet();
+        return itemReader;
+
+    }
+
+    private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("id", "name", "age", "address");
+        lineMapper.setLineTokenizer(tokenizer);
+
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            int id = fieldSet.readInt("id");
+            String name = fieldSet.readString("name");
+            String age = fieldSet.readString("age");
+            String address = fieldSet.readString("address");
+
+            return new Person(id, name, age, address);
+        });
+
+        FlatFileItemReader<Person> itemReader = new FlatFileItemReaderBuilder<Person>()
+            .name("csvFileItemReader")
+            .encoding("UTF-8")
+            .resource(new ClassPathResource(("test.csv")))
+            .linesToSkip(1)
+            .lineMapper(lineMapper)
+            .build();
+
+        itemReader.afterPropertiesSet();
+        return itemReader;
+    }
+
     private ItemWriter<Person> itemWriter() {
         return items -> log.info(items.stream()
-                .map(Person::getName)
-                .collect(Collectors.joining(", ")));
+            .map(Person::getName)
+            .collect(Collectors.joining(", ")));
     }
 
     private List<Person> getItems() {
